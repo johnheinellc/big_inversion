d53ee2829490a8980bdcbe2bdf8d69c8b406b420
johnheinellc <johnheinellc@gmail.com>
2022-03-10 17:20:10 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import PersonCard from './components/PersonCard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <PersonCard firstName = {"John"}lastName = {"Heine"}age = {34}hairColor = {"Red"}></PersonCard>
+      <PersonCard firstName = {"Marry"}lastName = {"Blige"}age = {51}hairColor = {"Blonde"}></PersonCard>
+      <PersonCard firstName = {"Jimmy"}lastName = {"Hendrix"}age = {80}hairColor = {"Black"}></PersonCard>
+      <PersonCard firstName = {"Frank"}lastName = {"Sinatra"}age = {107}hairColor = {"Brown"}></PersonCard> */}
+      <PersonCard firstName="John" lastName="Smith" age={8} hairColor="Brown" />
+      <PersonCard firstName="Jane" lastName="Doe" age={18} hairColor="Red" />
+      <PersonCard firstName="Mark" lastName="Clark" age={34} hairColor="Black" />
+      <PersonCard firstName="Sam" lastName="Bo" age={78} hairColor="Grey" />
+
+
     </div>
   );
 }
